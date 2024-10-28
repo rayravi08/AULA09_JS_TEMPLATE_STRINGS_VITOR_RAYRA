0049f663667964602a82b19ab824162e5ddbe592
rayravi08 <vitor.rayra123@gmail.com>
2024-10-28 08:24:15 -0300

    Add files via upload

---
+++ AULA09_JS_TEMPLATE_STRINGS_VITOR_RAYRA/script.js
@@ -0,0 +1,10 @@
+let nome = "Vitor";
+let sobreNome = "Manrtins";
+let idade = 16;
+
+let mensagem =
+  "Meu nome é " + nome + " " + sobreNome + " e minha idade " + idade + " anos.";
+console.log(mensagem);
+
+let mensagem2 = ` Meu nome é ${nome} ${sobreNome} e minha idade ${idade} anos.`;
+console.log(mensagem2);
